c435af27f2a577d22b2ccb7fbe4bbaa02ffb06e1
Conrad Eggan <conrade@redcatmfg.com>
2019-11-13 20:31:58 -0600

    cleanup

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 
 class TempForm(FlaskForm):
     username = StringField('Name', validators=[DataRequired()])
     machine = StringField('Machine', validators=[DataRequired()])
     maintemp = DecimalField('Main Temperature', validators=[DataRequired()])
-    auxtemp = DecimalField('Aux Temperature')
+    auxtemp = DecimalField('Aux Temperature', validators=[Optional()])
     submit = SubmitField('Submit')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from app import app
-# Add
 from flask import render_template
 from app.forms import TempForm
 
@@ -7,11 +6,35 @@ from app.forms import TempForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return (
+        """
+        <div>
+            Cambria Temperature Monitoring:
+            <a href="/logtemps">Log Temps</a>
+        </div>
+        <h1>Hello, Cambria!</h1>
+        """
+    )
 
 
 # Add
 @app.route('/logtemps', methods=['GET', 'POST'])
 def logtemps():
-    form = TempForm
+    form = TempForm()
+    if form.validate_on_submit():
+        return (
+            f"""
+            <div>
+                Cambria Temperature Monitoring: 
+                <a href="/index">Home</a> 
+                <a href="/logtemps">Log Temps</a>
+            </div>
+                <h1>You Entered</h1>
+            <p>Name: {form.username.data}</p>
+            <p>Machine: {form.machine.data}</p>
+            <p>Main Temp: {form.maintemp.data}</p>
+            <p>Aux Temp: {form.auxtemp.data}</p>
+            <button onclick="window.location.href = '/logtemps';">Continue</button>
+            """
+        )
     return render_template('temps.html', title='Temp Logs', form=form)
